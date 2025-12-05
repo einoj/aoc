0d260af3aac4d0aafa97d48db77821014669d59f
Eino Juhani Oltedal <ejo@akkodis.no>
2025-12-05 16:41:54 +0100

    2025: Day1 part 2

--- 2025/day1.c
+++ 2025/day1.c
@@ -7,25 +7,37 @@ int main(int argc, char* argv[]) {
     char s[5];
     int sum = 50;
     int password = 0;
+    int password_part2 = 0;
+    int val;
     while (scanf("%s", s)!=EOF) {
+        val =  strtol(&s[1],NULL,10);
         switch (s[0]) {
             case 'L':
-                sum -= strtol(&s[1],NULL,10);
-                while (sum < 0)
-                    sum+=100;
-                printf("%d\n",sum);
+                while (val > 0) {
+                    sum-=1;
+                    if (sum < 0)
+                        sum = 99;
+                    if (sum == 0)
+                        password_part2++;
+                    val--;
+                }
                 break;
             case 'R':
-                sum += strtol(&s[1],NULL,10);
-                while (sum > 99)
-                    sum-=100;
-                printf("%d\n",sum);
+                while (val > 0) {
+                    sum+=1;
+                    if (sum == 100)
+                        sum = 0;
+                    if (sum == 0)
+                        password_part2++;
+                    val--;
+                }
                 break;
         }
         if (sum == 0)
             password++;
     }
     printf("password = %d\n",password);
+    printf("password part2 = %d\n",password_part2);
 
     return 0;
 }
