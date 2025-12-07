f6010b83d3cede61ffa9200a80979ea8d10fbae0
Eino J. Oltedal <einoju@gmail.com>
2025-12-07 17:35:31 +0100

    2025: Day2 speed up
    5x speed up (from 4.2s to 850ms) by moving work out
    from the inner loop and stopping early if possible.

--- 2025/day2.cpp
+++ 2025/day2.cpp
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <stdbool.h>
 #include <unordered_set>
+#include <cassert>
 
-long int sum_of_invalidids(long int x, int digits, int divisor) {
+long int sum_of_invalidids(long int x, int digits, int divisor, char *number) {
     if (digits%divisor != 0)
         return 0;
-
-    long dh, dl;
-    char s[100];
-    char s2[50];
-    char s3[50];
-    int digits_to_compare=1;
-    if (divisor>digits_to_compare)
-        digits_to_compare=divisor;
-    sprintf(s, "%ld", x);
-    snprintf(s2, digits_to_compare+1, "%s",s);
-    dh = atoi(s2);
-    for (int i = digits_to_compare; i< digits; i+=digits_to_compare) {
-        snprintf(s3, digits_to_compare+1, "%s",&s[i]);
-        dl = atoi(s3);
-        if (dl != dh) {
+    for (int i = divisor; i< digits; i+=divisor) {
+        if (strncmp(&number[0],&number[i],divisor) != 0) {
             return 0;
         }
     }
@@ -29,32 +18,46 @@ long int sum_of_invalidids(long int x, int digits, int divisor) {
 }
 
 int main(int argc, char* argv[]) {
-    long int i,j,x,k, sum=0, sum_part2=0, tmp;
+    long int i,j,k, sum=0, sum_part2=0, tmp;
     int digits, half_digits;
+    char number[20];
     std::unordered_set<long> unique_ids;
-    std::unordered_set<long> unique_ids_part1;
     while(scanf("%ld-%ld,", &i, &k) != EOF) {
-        for (x = i; x <= k; x++) {
+        for (long int x = i; x <= k; x++) {
+            if (x<11)
+                continue;
             digits = lrint(floor(log10(x)+1));
             half_digits = digits/2;
-            for (j = 1; j <= half_digits; j++) {
-                tmp = sum_of_invalidids(x,digits,j);
+            sprintf(number, "%ld", x);
+            j=2;
+            for (int l = 0; l < digits-1; l++) {
+                if(number[l] != number[l+1]) {
+                    goto loop;
+                }
+            }
+            unique_ids.insert(x);
+            if(digits == 2){
+                sum+=x;
+                continue;
+            }
+            j=half_digits;
+loop:       for (j = j; j <= half_digits; j++) {
+                tmp = sum_of_invalidids(x,digits,j,number);
                 if (tmp == 0)
                     continue;
                 unique_ids.insert(tmp);
                 if(j==half_digits && digits%2 == 0)
-                    unique_ids_part1.insert(tmp);
+                    sum+=tmp;
             }
             for (auto itr = unique_ids.begin(); itr != unique_ids.end(); itr++)
                 sum_part2+= *itr;
-            for (auto itr = unique_ids_part1.begin(); itr != unique_ids_part1.end(); itr++)
-                sum+= *itr;
             unique_ids.clear();
-            unique_ids_part1.clear();
         }
     }
     printf("sum: %ld\n", sum);
     printf("sum_part2: %ld\n", sum_part2);
+    assert(sum == 13919717792);
+    assert(sum_part2 == 14582313461);
 
     return 0;
 } 
