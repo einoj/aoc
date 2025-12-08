d397e340af820d4ffb619424a07aa040db1eeed2
Eino J. Oltedal <einoju@gmail.com>
2025-12-08 17:54:50 +0100

    2025: Day 2 part 2 speed up
    Convert back to C No need for unordered sets since the duplicates are
    always from the previous run though, so if the current call of
    sum_of_invalids returns the previous one, don't add it.

--- 2025/day2.cpp
+++ 2025/day2.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <stdbool.h>
-#include <unordered_set>
-#include <cassert>
 
 long int sum_of_invalidids(long int x, int digits, int divisor, char *number) {
     if (digits%divisor != 0)
@@ -21,22 +17,22 @@ int main(int argc, char* argv[]) {
     long int i,j,k, sum=0, sum_part2=0, tmp;
     int digits, half_digits;
     char number[20];
-    std::unordered_set<long> unique_ids;
+    long int prev=0;
     while(scanf("%ld-%ld,", &i, &k) != EOF) {
         for (long int x = i; x <= k; x++) {
             if (x<11)
                 continue;
-            digits = lrint(floor(log10(x)+1));
+            digits = sprintf(number, "%ld", x);
             half_digits = digits/2;
-            sprintf(number, "%ld", x);
             j=2;
             for (int l = 0; l < digits-1; l++) {
                 if(number[l] != number[l+1]) {
                     goto loop;
                 }
             }
-            unique_ids.insert(x);
-            if(digits == 2){
+            prev = x;
+            sum_part2 += x;
+            if(digits == 2 || digits%2 == 0){
                 sum+=x;
                 continue;
             }
@@ -45,19 +41,26 @@ loop:       for (j = j; j <= half_digits; j++) {
                 tmp = sum_of_invalidids(x,digits,j,number);
                 if (tmp == 0)
                     continue;
-                unique_ids.insert(tmp);
+                if (tmp != prev) {
+                    prev = tmp;
+                    sum_part2 += tmp;
+                }
                 if(j==half_digits && digits%2 == 0)
                     sum+=tmp;
             }
-            for (auto itr = unique_ids.begin(); itr != unique_ids.end(); itr++)
-                sum_part2+= *itr;
-            unique_ids.clear();
         }
     }
+
     printf("sum: %ld\n", sum);
     printf("sum_part2: %ld\n", sum_part2);
-    assert(sum == 13919717792);
-    assert(sum_part2 == 14582313461);
+    if (sum != 13919717792) {
+        printf("ERROR: in part 1");
+        return -1;
+    }
+    if (sum_part2 != 14582313461) {
+        printf("ERROR: in part 2");
+        return -1;
+    }
 
     return 0;
 } 
